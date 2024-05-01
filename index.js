ec61b374b9ebb1076b233cb9c3a9e05ce11232f6
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-05-01 16:01:41 -0700

    created linesBet function, results not being utalized atm

--- index.js
+++ index.js
@@ -19,5 +19,55 @@ const depositMoney = () => {
     return console.log(`$${depositAmount}`)
 }
 
+const linesBet = () => {
+    console.log("\n")
+    let numberOfLines = prompt("Do you want to bet on 1, 2, or all 3 lines? ")
+    numberOfLines = parseInt(numberOfLines)
 
-depositMoney()
+    if(numberOfLines == 3) {
+        return console.log(`You bet on all ${numberOfLines} lines`)
+    } 
+
+    else if(numberOfLines == 2) {
+        console.log("\n1 = top, 2 = middle, 3 = bottom\n")
+        let firstChoice = prompt("What is the first line you want to bet on? ")
+        firstChoice = parseInt(firstChoice)
+
+        if (firstChoice != 1 && firstChoice != 2 && firstChoice != 3) {
+            console.log("You have to pick between 1, 2, or 3")
+            return linesBet()
+        }
+
+        let secondChoice = prompt("What is the second line you want to bet on? ")
+        secondChoice = parseInt(secondChoice)
+        if (secondChoice != 1 && secondChoice != 2 && secondChoice != 3) {
+            console.log("You have to pick between 1, 2, or 3")
+            return linesBet()
+        }
+
+        if (firstChoice == secondChoice) {
+            console.log("Can't pick the same line twice, choose again.\n")
+            return linesBet()
+        }
+        
+        return console.log(`You bet on ${numberOfLines} lines and those lines were ${firstChoice} and ${secondChoice}`)
+    } 
+
+    else if(numberOfLines == 1) {
+        console.log("\n1 = top, 2 = middle, 3 = bottom\n")
+        let firstChoice = prompt("What is the line you want to bet on? ")
+        firstChoice = parseInt(firstChoice)
+
+        if (firstChoice != 1 && firstChoice != 2 && firstChoice != 3) {
+            console.log("You have to pick between 1, 2, or 3")
+            return linesBet()
+        }
+        return console.log(`You bet on line ${firstChoice}`)
+    } 
+
+    else return linesBet()
+}
+
+
+// depositMoney()
+linesBet()
